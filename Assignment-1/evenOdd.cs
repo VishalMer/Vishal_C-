3647fccfcc5b66cfce6074f7866a290e56cde3c0
VishalMer <vishalmer99747@gmail.com>
2025-01-06 13:56:47 +0530

    Added 2 Programs

---
+++ Assignment-1/evenOdd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4BCAA.Assignment_1
+{
+    internal class evenOdd
+    {
+        static void Main(string[] args)
+        {
+            int no;
+
+            Console.Write("Enter no to check : ");
+            no = Convert.ToInt32(Console.ReadLine());
+
+            if (no % 2 == 0)
+            {
+                Console.WriteLine("The number " + no + " is EVEN.");
+            }
+            else
+            {
+                Console.WriteLine("The number " + no + " is ODD.");
+            }
+        }
+    }
+}
---
+++ Assignment-1/posNeg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4BCAA.Assignment_1
+{
+    internal class posNeg
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Enter number to check : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 0)
+            {
+                Console.WriteLine("The number is 0.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("The number " + num + " is NEGATIVE.");
+            }
+            else
+            {
+                Console.WriteLine("The number " + num + " is POSITIVE.");
+            }
+        }
+    }
+}
